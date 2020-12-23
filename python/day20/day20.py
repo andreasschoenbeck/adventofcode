64f99f6bd9cb5877e21fb66c1333367bca59d1c9
Andreas Schönbeck <a.schoenbeck@tricentis.com>
2020-12-23 19:19:13 +0100

    day20, part1 + plan for part2

--- python/day20/day20.py
+++ python/day20/day20.py
@@ -1,6 +1,8 @@
 import re
+import math
 
 edges = dict()
+matchingEdges = dict()
 tiles = dict()
 
 def readInput():
@@ -19,15 +21,60 @@ def readTile(tile):
     match = re.match("Tile ([0-9]*):", lines.pop(0))
     id = match[1]
 
-    tiles[id] = 0 # matching edgecount
+    matchingEdges[id] = [] 
 
     edges[(id,0)] = lines[0]
     edges[(id,1)] = ''.join(map(lambda s: s[-1], lines))
     edges[(id,2)] = lines[-1]
     edges[(id,3)] = ''.join(map(lambda s: s[0], lines))
-    
-    
 
+    tiles[id] = tile[1:]
+
+def findMatchingEdges():
+    edgeKeys = list(edges.keys())
+    for i in range(len(edgeKeys)):
+        key = edgeKeys[i]
+        value = edges[key]
+
+        for j in range(i+1,len(edgeKeys)):
+            compareKey = edgeKeys[j]
+            compareValue = edges[compareKey]
+
+            if compareKey[0] == key:
+                # ignore the edges of the same tile
+                continue
+            else:
+                if value == compareValue or ''.join(reversed(value)) == compareValue:
+                    matchingEdges[key[0]].append((key, compareKey))
+                    matchingEdges[compareKey[0]].append((key, compareKey))
+
+def stitchImage(startingTile):
+    global matchingEdges
+
+    side = math.sqrt(len(matchingEdges))
+    neighbors = matchingEdges[startingTile]
+
+    
 
 
 readInput()
+findMatchingEdges()
+
+corners = list(filter(lambda t: len(matchingEdges[t])==2, matchingEdges))
+
+result = 1
+for corner in corners:
+    result *= int(corner)
+
+print("corner ids multiplied: " + str(result))
+
+# stitch tiles - start with first corner, then go left to right by following the tiles dict
+# rotate and flip as needed e.g. #1,1 -> #2,3 and #2,2 has no partner -> flip top to bottom
+# remove edge and add to picture
+# (show picture)
+# find seamonsters - hm.... probably manually see if there's a rotation/flip needed for the picture
+# then create a mask with offset tuples and make a compare method, send it all possible coordinates and replace all pixels with O if found
+# count remaining #...
+# uff
+
+stitchImage(startingTile=corners[0])
