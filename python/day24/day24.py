0a72e8525652acec777a604a2e902948d604d050
Andreas Schönbeck <a.schoenbeck@tricentis.com>
2021-01-04 21:53:49 +0100

    day24 - beginnings

---
+++ python/day24/day24.py
@@ -0,0 +1,13 @@
+# coordinates like this:
+#    ---
+#    32101234
+# -1 X X X
+#  0  X O X X
+#  1 X X X
+#  2
+
+def readInput():
+    lines = []
+
+    with open("C:\\data\\dotnet_projects\\adventofcode\\python\\day24\\input_02.txt", 'r') as f:
+        lines = f.readlines
